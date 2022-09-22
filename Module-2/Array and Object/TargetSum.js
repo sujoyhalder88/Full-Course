1266e4944af08a6b7ade131805e9aba24619aaab
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-09-22 17:31:33 +0530

    conditional statement

---
+++ Module-2/Array and Object/TargetSum.js
@@ -0,0 +1,10 @@
+/**
+ *  Given an array A with a number x. We need to write a function which takes array A and x as argument and return a pair
+	from the array A whose sum is equal to x. If no pair exists then return empty array.
+	Example :
+	Input: {0,1,2,-2,4,5,6}
+	Sum = 5
+	Output: {1,4}
+	There are many approaches to solve this problem
+
+ */
