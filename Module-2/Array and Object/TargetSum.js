588944dfde7f7ae38ca4646b2f0e1f404c6f0624
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-09-29 15:55:07 +0530

    target sum

--- Module-2/Array and Object/TargetSum.js
+++ Module-2/Array and Object/TargetSum.js
@@ -8,3 +8,24 @@
 	There are many approaches to solve this problem
 
  */
+function targetSum(arr, sum) {
+    arr.sort((a, b) => a - b);
+    let leftSide = 0;
+    let rightSide = arr.length - 1;
+    let occur = [];
+    while (leftSide < rightSide) {
+        if (arr[leftSide] + arr[rightSide] < sum) {
+            leftSide++;
+        } else if (arr[leftSide] + arr[rightSide] > sum) {
+            rightSide--;
+        } else {
+            occur.push(arr[leftSide]);
+            occur.push(arr[rightSide]);
+            return occur;
+        }
+    }
+    return occur;
+}
+let arr = [0, 1, 2, -2, 4, 5, 6];
+let sum = 8;
+console.log(targetSum(arr, sum));
