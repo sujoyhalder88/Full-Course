853ced9e734f4e43c8ab192e85ad6bfcb0707f97
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-10-16 23:19:39 +0530

    promises resolve and reject

--- Module-3/AdvanceAsynchronousProgram/Promise.js
+++ Module-3/AdvanceAsynchronousProgram/Promise.js
@@ -1,3 +1,25 @@
+//======== Promise resolve ========
+
+var promise = new Promise((resolve, reject) => {
+    let a = 10;
+    let b = 10;
+    if (a === b) {
+        resolve();
+    } else {
+        reject();
+    }
+});
+
+promise
+    .then(() => {
+        console.log(`success the program`);
+    })
+    .catch(() => {
+        console.log(`reject the prorram`);
+    });
+
+//============promises reject============
+
 var promise = new Promise((resolve, reject) => {
     let a = 10;
     let b = 9;
