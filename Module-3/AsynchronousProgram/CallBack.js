6ecc20e7bf904dbb4097d9109ed3c01cf79e0bd8
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-10-18 11:48:24 +0530

    callback function

--- Module-3/AsynchronousProgram/CallBack.js
+++ Module-3/AsynchronousProgram/CallBack.js
@@ -32,9 +32,33 @@ greet(() => console.log("Hi"));
 //     console.log("Second");
 // });
 
-console.log("Hi");
-function callback() {
-    console.log("budy");
-}
-setTimeout(callback, 2000);
-console.log("WellCome");
+///=======================================================
+// console.log("Hi");
+// function callback() {
+//     console.log("budy");
+// }
+// setTimeout(callback, 2000);
+// console.log("WellCome");
+
+//======================================================
+
+// const call = (call_2) => {
+//     call_2();
+//     console.log("1st");
+// };
+// const secondCall = () => {
+//     console.log("2nd");
+// };
+// call(secondCall);
+
+//==============make a ice crem========
+
+const order = (call_production) => {
+    console.log("order place , call the production");
+    call_production();
+};
+
+const production = () => {
+    console.log("order resived,start production");
+};
+order(production);
