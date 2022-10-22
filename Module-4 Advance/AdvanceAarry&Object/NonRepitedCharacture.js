68a41a569f8a6ebc23f6f6027fe132d76dee2af7
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-10-23 01:17:25 +0530

    Non repeated Characture

---
+++ Module-4 Advance/AdvanceAarry&Object/NonRepitedCharacture.js
@@ -0,0 +1,28 @@
+/**
+ *  First non-repeating character
+	Non-repeating – A character is known as a non-repeating character when its frequency is unit in a given string.
+	Problem – Given a string. You need to print the first non-repeating character. 
+
+	For example –
+	Input – str = “unacademy”
+	Output – u
+
+ */
+
+function nonRepeatedCharacture(str) {
+    let mp = {};
+    for (let i = 0; i < str.length; i++) {
+        if (!mp[str[i]]) {
+            mp[str[i]] = 1;
+        } else {
+            mp[str[i]] += 1;
+        }
+    }
+    for (let i = 0; i < str.length; i++) {
+        if (mp[str[i]] == 1) {
+            return str[i];
+        }
+    }
+    return mp;
+}
+console.log(nonRepeatedCharacture("unacademy"));
