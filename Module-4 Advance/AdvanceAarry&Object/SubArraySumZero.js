c3455ee5da1db27fb218c9ff5bec2e9119afcee4
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-10-25 15:03:17 +0530

    sub Array Sum Zero

--- Module-4 Advance/AdvanceAarry&Object/SubArraySumZero.js
+++ Module-4 Advance/AdvanceAarry&Object/SubArraySumZero.js
@@ -9,3 +9,51 @@
     Index 6 to 7
 
  */
+
+function subArrayWithZeroSum(arr) {
+  var map = new Map();
+  var n = arr.length;
+  var output = new Array();
+
+  var sum = 0;
+  for (var i = 0; i < n; i++) {
+    sum += arr[i];
+
+    if (sum == 0) {
+      const pair = {
+        key: 0,
+        value: i,
+      };
+      output.push(pair) > 0;
+    }
+    var tempArray = new Array();
+
+    if (map.has(sum)) {
+      tempArray = map.get(sum);
+      for (var j = 0; j < tempArray.length; j++) {
+        const pair = {
+          key: tempArray[j] + 1,
+          value: i,
+        };
+        output.push(pair) > 0;
+      }
+    }
+    tempArray.push(i) > 0;
+    map.set(sum, tempArray);
+  }
+  if (output.length == 0) {
+    console.log("No subarray having zero sum exists");
+  } else {
+    print(output);
+  }
+}
+
+function print(out) {
+  for (var i = 0; i < out.length; i++) {
+    var p = out[i];
+    console.log("Index " + p.key + " to " + p.value + "\n");
+  }
+}
+
+var arr = [0, 2, 4, -1, -3, 4, -5, 5];
+var output = subArrayWithZeroSum(arr);
