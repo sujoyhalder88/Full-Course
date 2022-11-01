285a18596aeb428ec32cee93f5981131ae48a5c6
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-11-01 23:04:57 +0530

    one more add select sort

--- Module-2/SortingAlgotithams/SelectionSort/SelectionSort.js
+++ Module-2/SortingAlgotithams/SelectionSort/SelectionSort.js
@@ -24,3 +24,26 @@ function selectionSort(inputArray) {
   return inputArray;
 }
 console.log(selectionSort([5, 3, 1, 2]));
+
+//======================================================
+
+function sSort(arr) {
+  let i;
+  let j;
+  let minIndex;
+  for (let i = 0; i < arr.length - 1; i++) {
+    minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex != i) {
+      let temp = arr[minIndex];
+      arr[minIndex] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  return arr;
+}
+console.log(sSort([14, 15, 6, 5, 14, 1]));
