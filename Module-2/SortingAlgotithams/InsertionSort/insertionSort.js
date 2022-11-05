60911e38d8878136ec4b86f845a540b829963a17
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-11-05 21:08:44 +0530

    insertion sort

--- Module-2/SortingAlgotithams/InsertionSort/insertionSort.js
+++ Module-2/SortingAlgotithams/InsertionSort/insertionSort.js
@@ -2,22 +2,38 @@
  *
  */
 
-function insertionSort(inputArr) {
-  let n = inputArr.length;
-  for (let i = 1; i < n; i++) {
-    // Choosing the first element of the unsorted array
-    let current = inputArr[i];
-    // The last element of our sorted subarray
-    let j = i - 1;
+// function insertionSort(inputArr) {
+//   let n = inputArr.length;
+//   for (let i = 1; i < n; i++) {
+//     // Choosing the first element of the unsorted array
+//     let current = inputArr[i];
+//     // The last element of our sorted subarray
+//     let j = i - 1;
 
-    // Find the position of the element in the sorted subarray
-    while (j > -1 && current < inputArr[j]) {
-      inputArr[j + 1] = inputArr[j];
+//     // Find the position of the element in the sorted subarray
+//     while (j > -1 && current < inputArr[j]) {
+//       inputArr[j + 1] = inputArr[j];
+//       j--;
+//     }
+
+//     inputArr[j + 1] = current;
+//   }
+//   return inputArr;
+// }
+// console.log(insertionSort([15, 12, 31, 14, 14, 17]));
+
+//=============================================
+
+function implementInsertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let newArray = arr[i];
+    let j = i - 1;
+    while (j > -1 && newArray < arr[j]) {
+      arr[j + 1] = arr[j];
       j--;
     }
-
-    inputArr[j + 1] = current;
+    arr[j + 1] = newArray;
   }
-  return inputArr;
+  return arr;
 }
-console.log(insertionSort([15, 12, 31, 14, 14, 17]));
+console.log(implementInsertionSort([25, 14, 30, 17, 48, 50]));
