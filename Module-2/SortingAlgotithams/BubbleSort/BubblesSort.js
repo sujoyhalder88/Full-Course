83d6d09a04fce1be74c40bc8582a082e25fe776e
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-11-05 17:02:21 +0530

    bubble sort basic

---
+++ Module-2/SortingAlgotithams/BubbleSort/BubblesSort.js
@@ -0,0 +1,19 @@
+/**
+ *  bubble sorts
+ */
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+    console.log(`iteration ${i + 1} :`, arr);
+  }
+  return arr;
+}
+
+console.log(bubbleSort([5, 3, 1, 2]));
