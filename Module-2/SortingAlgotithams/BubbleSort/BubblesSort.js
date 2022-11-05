e9653c71629af5e892ef6744911a6b48d201dd49
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-11-05 17:38:27 +0530

    bubble sort

--- Module-2/SortingAlgotithams/BubbleSort/BubblesSort.js
+++ Module-2/SortingAlgotithams/BubbleSort/BubblesSort.js
@@ -2,7 +2,24 @@
  *  bubble sorts
  */
 
-function bubbleSort(arr) {
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//     console.log(`iteration ${i + 1} :`, arr);
+//   }
+//   return arr;
+// }
+
+// console.log(bubbleSort([5, 3, 1, 2]));
+
+//============================================================
+function bubblesSortAllElement(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       if (arr[j] > arr[j + 1]) {
@@ -11,9 +28,8 @@ function bubbleSort(arr) {
         arr[j + 1] = temp;
       }
     }
-    console.log(`iteration ${i + 1} :`, arr);
+    console.log(`iteration ${i + 1}:`, arr);
   }
   return arr;
 }
-
-console.log(bubbleSort([5, 3, 1, 2]));
+console.log(bubblesSortAllElement([1, 4, 2, 5, -2, 3]));
