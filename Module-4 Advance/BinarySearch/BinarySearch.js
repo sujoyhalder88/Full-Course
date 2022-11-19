b21350dc7b3d739858f757a5015aaf4252ac446f
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-11-19 23:30:35 +0530

    binary Search

---
+++ Module-4 Advance/BinarySearch/BinarySearch.js
@@ -0,0 +1,18 @@
+/**
+ *  here are create a basic  Binery Search implement
+ */
+function binerySearch(arr, x) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] == x) return mid;
+    if (arr[mid] > x) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return undefined;
+}
+console.log(binerySearch([4, 3, 2, 1, 5, 6, 8, 9, 15], 2));
